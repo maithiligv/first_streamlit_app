9b7490280a8e3043914bac2b15cf59df5d55807a
maithiligv <149065132+maithiligv@users.noreply.github.com>
2023-11-12 00:03:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connectore
+import snowflake.connector
 
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
